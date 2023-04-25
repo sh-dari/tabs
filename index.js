f392b4521547696276593ad5498f8ab49ea7af90
Дарья Шипилова <sh.dari@list.ru>
2023-04-25 11:20:46 +0300

    fix: id

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const tabs = document.querySelectorAll('.tabs__list');
 const tabButtons = document.querySelectorAll('.tabs__btn');
 const continueButton = document.querySelector('.tabs__btn-continue');
 
-let buttonClicked = ['1'];
+let buttonClicked = [1];
 
-const handleTabClick = (evt) => {
+const handleTabClick = (index) => {
   tabButtons.forEach(tab => {
     tab.classList.remove('tabs__btn_active');
   });
-  const {id} = evt.currentTarget;
-  document.querySelector(`[id="tab-${id}"]`).classList.add('tabs__list_show');
-  document.querySelector(`[id="${id}"]`).classList.add('tabs__btn_active');
-  buttonClicked.push(id);
-  let intersection = ['1', '2', '3', '4'].filter(x => !buttonClicked.includes(x));
+  tabs.forEach(tab => {
+    tab.classList.remove('tabs__list_show');
+  });
+  const id = index;
+  tabs[index].classList.add('tabs__list_show');
+  tabButtons[index].classList.add('tabs__btn_active');
+  buttonClicked.push(id + 1);
+  console.log(buttonClicked)
+  let intersection = [1, 2, 3, 4].filter(x => !buttonClicked.includes(x));
   if (intersection.length == 0) {
     continueButton.disabled = false;
   }
 }
 
-tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
+tabButtons.forEach((button, index) => button.addEventListener('click', () => {handleTabClick(index)}));
